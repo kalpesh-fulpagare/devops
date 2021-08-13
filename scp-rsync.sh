0cd80c134fd52d5e8908636e49e35c2b464f64cc
Kalpesh Fulpagare <kalpesh.fulpagare@gmail.com>
2021-08-13 17:30:08 +0530

    Added rsync commands

---
+++ scp-rsync.sh
@@ -0,0 +1,5 @@
+# EMM
+scp -P 911 ril-deployment.tar.gz deploy@10.130.31.113:/var/apps/downloads
+rsync --progress -rt kalpesh@192.168.100.1:/home/kalpesh/projects/ /home/kalpesh/projects/ 
+rsync --progress -rt --exclude 'abc.txt' kalpesh@192.168.100.1:/home/kalpesh/projects/ /home/kalpesh/projects/ 
+rsync --progress -rt --exclude 'configurations' kalpesh@192.168.100.1:/home/kalpesh/projects/ /home/kalpesh/projects/ 
--- scp.sh
+++
@@ -1,2 +0,0 @@
-# EMM
-scp -P 911 ril-deployment.tar.gz deploy@10.130.31.113:/var/apps/downloads
