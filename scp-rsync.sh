2d97ff69f9dd00befd56c8c20b60e4305f0a82b0
Kalpesh Fulpagare <kalpesh.fulpagare@gmail.com>
2021-09-30 20:16:42 +0530

    Added one more syntax for rsync command

--- scp-rsync.sh
+++ scp-rsync.sh
@@ -3,3 +3,4 @@ scp -P 911 ril-deployment.tar.gz deploy@10.130.31.113:/var/apps/downloads
 rsync --progress -rt kalpesh@192.168.100.1:/home/kalpesh/projects/ /home/kalpesh/projects/ 
 rsync --progress -rt --exclude 'abc.txt' kalpesh@192.168.100.1:/home/kalpesh/projects/ /home/kalpesh/projects/ 
 rsync --progress -rt --exclude 'configurations' kalpesh@192.168.100.1:/home/kalpesh/projects/ /home/kalpesh/projects/ 
+rsync -avzh -e 'ssh -p 911' --progress shared/vendor kalpesh@192.168.100.1:/var/www/demo-project/shared/
