f400a220cea95f21e5c7321a700524caa0e0348f
Kalpesh Fulpagare <kalpesh.fulpagare@gmail.com>
2023-04-14 20:22:53 +0530

    Update postgres-v1.sql

--- postgres-v1.sql
+++ postgres-v1.sql
@@ -5,6 +5,8 @@ WHERE cardinality(pg_blocking_pids(pid)) > 0;
 
 # Postgresql DeadTuple count of top tables
 SELECT n_live_tup, n_dead_tup, relname FROM pg_stat_all_tables ORDER BY n_dead_tup DESC LIMIT 25;
+SELECT relname, n_live_tup, n_dead_tup, round((n_dead_tup/n_live_tup::float::numeric)*100, 2) perc_dead_tuple 
+FROM pg_stat_all_tables WHERE n_live_tup > 1 ORDER BY n_dead_tup DESC LIMIT 25;
 
 # View query details from PID
 SELECT pid, query, now() - pg_stat_activity.query_start AS duration, pg_stat_activity.query_start AS start_time, state 
